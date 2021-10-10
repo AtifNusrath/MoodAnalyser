50e75d3e522b364327dfc508f7f208594d7e3df8
atif00 <mohammedatif90@gmail.com>
2021-10-10 19:45:13 +0530

    Respond Happy or Sad Mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -4,4 +4,12 @@ public class MoodAnalyser {
     public static void main(String[] args) {
         System.out.println("Mood Analyzer Problem");
     }
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad")){
+            return "SAD";
+        }else{
+            return "HAPPY";
+        }
+    }
 }
